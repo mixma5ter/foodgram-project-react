aea9cdb684a29dfdfb07e74127d7f0cd0089915b
Евгений Спиридонов <deejay999@yandex.ru>
2022-11-16 18:22:18 +0300

    add apps

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,6 +26,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api.apps.ApiConfig',
+    'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
