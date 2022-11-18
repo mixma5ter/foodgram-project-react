0d9160da2493933a109383b67e8bebd6a9a62982
Евгений Спиридонов <deejay999@yandex.ru>
2022-11-18 12:28:08 +0300

    add management command

---
+++ backend/recipes/management/commands/load_ingridients.py
@@ -0,0 +1,22 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Загрузка ингредиентов в БД из .csv'
+
+    def handle(self, **kwargs):
+        with open(
+            f'../data/ingredients.csv',
+            'r',
+            encoding='UTF-8'
+        ) as file:
+            reader = csv.reader(file, delimiter=',')
+            for row in reader:
+                Ingredient.objects.get_or_create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты загружены в БД'))
