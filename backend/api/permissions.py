a3e44eef9cf5506a52104e9fc1bb2cb87d1ba752
Евгений Спиридонов <deejay999@yandex.ru>
2022-11-30 15:26:41 +0300

    update permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -14,3 +14,15 @@ class RecipePermission(permissions.BasePermission):
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or (
+                        request.user.is_authenticated
+                        and (
+                                obj.author == request.user
+                                or request.user.is_superuser
+                        )
+                )
+        )
